bb5fb98bb27a488c4362c1ac90996cf9825d9928
Magic <jameswalsh5683@gmail.com>
2015-06-17 20:33:33 +0100

    Launch GTA, Close launcher
    Added the option to launch gta when the prog is launch, and to close the
    launcher after the program is loaded.

--- GTAV Prioritizer/Program.cs
+++ GTAV Prioritizer/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.IO;
 
 namespace GTAV_Prioritizer
 {
@@ -11,10 +12,130 @@ namespace GTAV_Prioritizer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Grand Theft Auto Prioritizer by Magic");
+            bool skiptheBS = false;
+            skiptheBS = Properties.Settings.Default.SkipBS;
+            if (File.Exists("noskip.txt"))
+            {
+                skiptheBS = false;
+                Console.WriteLine("noskip.txt found...");
+            }
+            else if (skiptheBS)
+            {
+                Console.WriteLine("Program already configured, continuing...");
+            }
+            else
+            {
+                Console.WriteLine("Program not yet configured or users wants popup, configuring...");
+            }
+
+            #region configuration
+            if (skiptheBS == false)
+            {
+                //should we close launcher
+                string ToCloseLauncher = "";
+                while (!(ToCloseLauncher == "y" | ToCloseLauncher == "n"))
+                {
+                    Console.WriteLine("Do you wish to close GTAVLauncher.exe when the game is loaded? y/n (may cause crashing)");
+                    try
+                    {
+                        ToCloseLauncher = Console.ReadLine();
+                    }
+                    catch (Exception) { }
+
+                }
+
+                if (ToCloseLauncher == "y")
+                {
+                    Properties.Settings.Default.CloseGTALauncher = true;
+                }
+                else
+                {
+                    Properties.Settings.Default.CloseGTALauncher = false;
+                }
+
+                //should we start gta from here
+                Console.WriteLine("Do you wish to start gtav every time this program is launched? y/n");
+
+                string StartGTA;
+                StartGTA = Console.ReadLine();
+                if (StartGTA == "y")
+                {
+                    Properties.Settings.Default.LaunchGTA = true;
+                }
+                else
+                {
+                    Properties.Settings.Default.LaunchGTA = false;
+                }
+
+            }
+            Properties.Settings.Default.Save();
+            #endregion
+
+            //rungta!
+            if (Properties.Settings.Default.LaunchGTA == true)
+                LaunchGTA();
+            //prioritize it!
+            Prioritize();
+            //wait for game to load social club
+            Console.WriteLine("Waiting for game to load social club");
+            System.Threading.Thread.Sleep(10000);
+            //close launcher!
+            if (Properties.Settings.Default.CloseGTALauncher == true)
+                CloseLauncher();
+
+            //we're done
+            Console.WriteLine("Program Finished, exiting...");
+            System.Threading.Thread.Sleep(10000);
+            Environment.Exit(1);
+        }
+        static void CloseLauncher()
+        {
+            string GTAVLauncherexe = "GTAVLauncher";
+            Console.WriteLine("Attempting to close launcher...");
+            Process[] RunningShit = Process.GetProcessesByName(GTAVLauncherexe);
+            if (RunningShit.Length > 0)
+            {
+                foreach (Process GTAVLauncher in RunningShit)
+                {
+                    Console.WriteLine("Process found. [ Name: {0} | ID: {1} | Prio: {2} ]", GTAVLauncher.ProcessName, GTAVLauncher.Id, GTAVLauncher.PriorityClass);
+
+                    Console.WriteLine("Killing...");
+                    try
+                    {
+                        GTAVLauncher.Kill();
+                        Console.WriteLine("Process killed");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No GTAVlauncher found...");
+            }
+
+        }
+        static void LaunchGTA()
+        {
+            //steam://run/271590
+            Console.WriteLine("Booting GTA");
+            try
+            {
+                System.Diagnostics.Process.Start("steam://run/271590");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void Prioritize()
+        {
             string GTAVexe = "gta5";
             bool found = false;
-
-            Console.WriteLine("GTAV Prioritizer by Magic");
 
             while (found == false)
             {
@@ -68,11 +189,8 @@ namespace GTAV_Prioritizer
                 }
             }
 
-            //and we're done
-            Console.WriteLine("Exiting...");
-            System.Threading.Thread.Sleep(5000);
-            Environment.Exit(1);
 
         }
     }
+
 }
